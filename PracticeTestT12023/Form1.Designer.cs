271f9688201a09de8a0ea303a8fd4101fc41cb59
johanwitt <johan.witt23@gmail.com>
2023-06-14 11:18:23 +1200

    Add project files.

---
+++ PracticeTestT12023/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace PracticeTestT12023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.practiceTest2023DataSet = new PracticeTestT12023.PracticeTest2023DataSet();
+            this.companyDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.company_DetailsTableAdapter = new PracticeTestT12023.PracticeTest2023DataSetTableAdapters.Company_DetailsTableAdapter();
+            this.companyNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.industryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yearRevenueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.revenueGrowthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numberofEmployeesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.headquartersDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.foundationdateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.practiceTest2023DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyDetailsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(107, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(371, 48);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(371, 106);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(371, 171);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(607, 48);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Company Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Industry";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Year Revenue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Revenue Growth";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "No. of Employees";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(269, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Headquarters";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(515, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Foundation Date";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 29);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 29);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Pos Growth";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(400, 221);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 29);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Ave Rev";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(518, 221);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 29);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Auto Ind";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(645, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 29);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Foundation";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.companyNameDataGridViewTextBoxColumn,
+            this.industryDataGridViewTextBoxColumn,
+            this.yearRevenueDataGridViewTextBoxColumn,
+            this.revenueGrowthDataGridViewTextBoxColumn,
+            this.numberofEmployeesDataGridViewTextBoxColumn,
+            this.headquartersDataGridViewTextBoxColumn,
+            this.foundationdateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.companyDetailsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 315);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(739, 216);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // practiceTest2023DataSet
+            // 
+            this.practiceTest2023DataSet.DataSetName = "PracticeTest2023DataSet";
+            this.practiceTest2023DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // companyDetailsBindingSource
+            // 
+            this.companyDetailsBindingSource.DataMember = "Company_Details";
+            this.companyDetailsBindingSource.DataSource = this.practiceTest2023DataSet;
+            // 
+            // company_DetailsTableAdapter
+            // 
+            this.company_DetailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // companyNameDataGridViewTextBoxColumn
+            // 
+            this.companyNameDataGridViewTextBoxColumn.DataPropertyName = "Company_Name";
+            this.companyNameDataGridViewTextBoxColumn.HeaderText = "Company_Name";
+            this.companyNameDataGridViewTextBoxColumn.Name = "companyNameDataGridViewTextBoxColumn";
+            // 
+            // industryDataGridViewTextBoxColumn
+            // 
+            this.industryDataGridViewTextBoxColumn.DataPropertyName = "Industry";
+            this.industryDataGridViewTextBoxColumn.HeaderText = "Industry";
+            this.industryDataGridViewTextBoxColumn.Name = "industryDataGridViewTextBoxColumn";
+            // 
+            // yearRevenueDataGridViewTextBoxColumn
+            // 
+            this.yearRevenueDataGridViewTextBoxColumn.DataPropertyName = "Year_Revenue";
+            this.yearRevenueDataGridViewTextBoxColumn.HeaderText = "Year_Revenue";
+            this.yearRevenueDataGridViewTextBoxColumn.Name = "yearRevenueDataGridViewTextBoxColumn";
+            // 
+            // revenueGrowthDataGridViewTextBoxColumn
+            // 
+            this.revenueGrowthDataGridViewTextBoxColumn.DataPropertyName = "Revenue_Growth";
+            this.revenueGrowthDataGridViewTextBoxColumn.HeaderText = "Revenue_Growth";
+            this.revenueGrowthDataGridViewTextBoxColumn.Name = "revenueGrowthDataGridViewTextBoxColumn";
+            // 
+            // numberofEmployeesDataGridViewTextBoxColumn
+            // 
+            this.numberofEmployeesDataGridViewTextBoxColumn.DataPropertyName = "Number_of_Employees";
+            this.numberofEmployeesDataGridViewTextBoxColumn.HeaderText = "Number_of_Employees";
+            this.numberofEmployeesDataGridViewTextBoxColumn.Name = "numberofEmployeesDataGridViewTextBoxColumn";
+            // 
+            // headquartersDataGridViewTextBoxColumn
+            // 
+            this.headquartersDataGridViewTextBoxColumn.DataPropertyName = "Headquarters";
+            this.headquartersDataGridViewTextBoxColumn.HeaderText = "Headquarters";
+            this.headquartersDataGridViewTextBoxColumn.Name = "headquartersDataGridViewTextBoxColumn";
+            // 
+            // foundationdateDataGridViewTextBoxColumn
+            // 
+            this.foundationdateDataGridViewTextBoxColumn.DataPropertyName = "Foundation_date";
+            this.foundationdateDataGridViewTextBoxColumn.HeaderText = "Foundation_date";
+            this.foundationdateDataGridViewTextBoxColumn.Name = "foundationdateDataGridViewTextBoxColumn";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label8.Location = new System.Drawing.Point(618, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "YYYY-MM-DD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 575);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.practiceTest2023DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyDetailsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PracticeTest2023DataSet practiceTest2023DataSet;
+        private System.Windows.Forms.BindingSource companyDetailsBindingSource;
+        private PracticeTest2023DataSetTableAdapters.Company_DetailsTableAdapter company_DetailsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn companyNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn industryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearRevenueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn revenueGrowthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numberofEmployeesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn headquartersDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn foundationdateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ PracticeTestT12023/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeTestT12023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=PracticeTest2023;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'practiceTest2023DataSet.Company_Details' table. You can move, or remove it, as needed.
+            this.company_DetailsTableAdapter.Fill(this.practiceTest2023DataSet.Company_Details);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // submit button
+        {
+            conn.Open(); // opens database
+
+            // insert into Company_Details values ('Toyota, Automotive, 223, 6, 67000, Tokyo, 2/2/1922')
+
+            string comm = "insert into Company_Details values ('" + textBox1.Text + "','" + textBox2.Text + "','" + Convert.ToInt32(textBox3.Text) + "','" + Convert.ToDecimal(textBox4.Text) + "','" + Convert.ToInt32(textBox5.Text) + "','" + textBox6.Text + "','" + Convert.ToDateTime(textBox7.Text) + "')";
+
+            // SqlCommand class
+
+            SqlCommand objcomm = new SqlCommand(comm, conn); // created command obj
+
+            objcomm.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("Data entry done!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open(); // opens database
+
+            // select * from Company_Details
+
+            string query = "select * from Company_Details";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+
+            DataSet data = new DataSet();
+
+            da.Fill(data);
+            conn.Close();
+
+            dataGridView1.DataSource = data.Tables[0]; // display into the datagridview
+        }
+    }
+}
